5ff6c2d8329b65543c7208e352dd5bb18484ce00
Asbjørn Steinskog <asbjorn@steinskog.me>
2016-04-06 11:14:13 +0200

    More API stuff

--- api.py
+++ api.py
@@ -11,24 +11,35 @@ class MainHandler(tornado.web.RequestHandler):
 		self.set_header('Access-Control-Allow-Origin', '*')
 		self.set_header('Content-Type', 'application/json')
 
-		r = stuff("29jan_tweets.json")
+		passes = self.get_argument("passes", 1, True)
+		num_topics = self.get_argument("num_topics", 0, True)
+		json_file=self.get_argument("json", "29jan_tweets", True)
 
-		self.write(json.dumps(r, indent=4, ensure_ascii=False))
+		if not "." in json_file:
+			json_file += ".json"
+
+		topics = run_lda(json_file, int(passes), int(num_topics))
+
+		response = {"file": json_file, "topics": topics, "num_topics": len(topics.keys()), "passes": passes}
+
+		self.write(json.dumps(response, indent=4, ensure_ascii=False))
 
 def app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/lda", MainHandler),
     ])
 
-def stuff(file_name):
+def run_lda(file_name, passes, num_topics):
 	dictionary, corpus = create_bag_of_words(file_name)
-	num_topics = 10 # default
 
-	create_lsi_model(file_name)
-	num_topics = find_number_of_topics(file_name)
+	if not num_topics:
+		print("Number of topics not provided. Finding optimal number of topics by using the Elbow method.")
+		create_lsi_model(file_name)
+		num_topics = find_number_of_topics(file_name)
+
 	print "=== Using " + str(num_topics) + " topics ==="
 
-	return generate_topics(num_topics, corpus, dictionary)
+	return generate_topics(num_topics, corpus, dictionary, passes)
 
 
 if __name__ == "__main__":
